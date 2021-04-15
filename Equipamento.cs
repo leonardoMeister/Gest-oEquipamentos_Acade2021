83f2b3cf59a1e55d18d004a0865957b71b884de1
Leonardo Meister <leozinhomm1@gmail.com>
2021-04-15 16:42:15 -0300

    Adicionar arquivos de projeto.

---
+++ Equipamento.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestãoEquipamentos_Acade2021
+{
+    class Equipamento
+    {
+        private float preco;
+        private string nome;
+        private int serie;
+        private string data;
+        private string fabricante;
+        private int id;
+
+        public float Preco { get => preco; set => preco = value; }
+        public string Nome { get => nome; set => nome = value; }
+        public int Serie { get => serie; set => serie = value; }
+        public string Data { get => data; set => data = value; }
+        public string Fabricante { get => fabricante; set => fabricante = value; }
+        public int Id { get => id; set => id = value; }
+
+        public Equipamento(float preco, string nome, int serie, string data, string fabricante)
+        {
+            this.Preco = preco;
+            this.Nome = nome;
+            this.Serie = serie;
+            this.Data = data;
+            this.Fabricante = fabricante;
+        }
+      
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestãoEquipamentos_Acade2021
+{
+    class Program
+    {
+        public Program()
+        {
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            new Program();
+        }
+    }
+}
