fa284774521d9effac42e97a6d5d528abac30022
Leonardo Meister <leozinhomm1@gmail.com>
2021-04-16 14:55:41 -0300

    Atualização Menu e Tratamentos.

--- Program.cs
+++ Program.cs
@@ -11,10 +11,89 @@ namespace GestãoEquipamentos_Acade2021
         Equipamento[] list = new Equipamento[10];
         public Program()
         {
-            
+            while (0 == 0)
+            {
+                string nome;
+                string serie;
+                try
+                {
+                    switch (OperacaoDesejada())
+                    {
+                        case 1:
+                            CriarEquipamento();
+                            break;
+                        case 2:
+                            Console.WriteLine("Informe o Nome do Equipamento para Remover: ");
+                            nome = Console.ReadLine();
+                            Console.WriteLine("Informe o Número de série do Equipamento: ");
+                            serie = Console.ReadLine();
+                            if (VerificarExistenciaEquipamento(nome, serie))
+                            {
+                                RemoverEquipamento(nome, serie);
+                                Console.WriteLine("Equipamento Removido com sucesso!");
+                            }
+                            else Console.WriteLine("Equipamento não existente ou não localizado!");
+                            break;
+                        case 3:
+                            MostrarEquipamentos();
+                            break;
+                        case 4:
+                            Console.WriteLine("Informe o Nome do Equipamento para Pesquisar: ");
+                            nome = Console.ReadLine();
+                            Console.WriteLine("Informe o Número de série do Equipamento: ");
+                            serie = Console.ReadLine();
 
-            Console.Read();
+                            if (VerificarExistenciaEquipamento(nome, serie))
+                            {
+                                Equipamento equipamentoSelecionado = BuscarEquipamento(nome, serie);
+                                Console.WriteLine("Equipamento Localizado!");
+                                Console.WriteLine("------------------------------------");
+                                Console.WriteLine($"Nome do Equipamento: {equipamentoSelecionado.Nome}");
+                                Console.WriteLine($"Preço do Equipamento: {equipamentoSelecionado.Preco}");
+                                Console.WriteLine($"Data do Equipamento: {equipamentoSelecionado.Data}");
+                                Console.WriteLine($"Fabricante do Equipamento: {equipamentoSelecionado.Fabricante}");
+                                Console.WriteLine($"Número Série do Equipamento: {equipamentoSelecionado.Serie}");
+                                Console.WriteLine($"ID do Equipamento: {equipamentoSelecionado.Id}");
 
+                            }
+                            else Console.WriteLine("Equipamento não existente ou Não Localizado!");
+
+                            break;
+                        case 5:
+                            Console.WriteLine("Informe o Nome do Equipamento para Pesquisar: ");
+                            nome = Console.ReadLine();
+                            Console.WriteLine("Informe o Número de série do Equipamento: ");
+                            serie = Console.ReadLine();
+                            if (VerificarExistenciaEquipamento(nome, serie))
+                            {
+                                EditarEquipamento(BuscarEquipamento(nome, serie));
+                            }
+                            else Console.WriteLine("Equipamento Não existente ou Não Localizado!");
+                            break;
+
+                        default:
+                            Console.WriteLine("Valor inválido");
+                            break;
+                    }
+                    Console.WriteLine("Press Entrer to continue!");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+                catch (Exception)
+                {
+
+                    Console.WriteLine("Opção Inválida!");
+                }
+            }
+        }
+        public int OperacaoDesejada()
+        {
+            Console.WriteLine("Selecione e Operação Desejada:\n[1] Adicionar Equipamento\n[2] Remover Equipamento\n" +
+                              "[3] Mostrar Equipamentos\n[4] Visualizar um Equipamento\n[5] Editar Equipamento");
+            int aux = Convert.ToInt32(Console.ReadLine());
+
+            Console.Clear();
+            return aux;
         }
 
         public void MostrarEquipamentos()
@@ -38,26 +117,43 @@ namespace GestãoEquipamentos_Acade2021
         }
         public bool VerificarExistenciaEquipamento(string nome, string numeroSerie)
         {
-            for (int i = 0; i < list.Length; i++)
+            try
             {
-                if (list[i].Nome == nome && list[i].Serie == Convert.ToInt32(numeroSerie))
+                for (int i = 0; i < list.Length; i++)
                 {
-                    return true;
+                    if (list[i].Nome == nome && list[i].Serie == Convert.ToInt32(numeroSerie))
+                    {
+                        return true;
+                    }
                 }
+                return false;
+
             }
-            return false;
+            catch (Exception)
+            {
+                Console.WriteLine("Valor Invalido");
+                return false;
+            }
         }
 
         public Equipamento BuscarEquipamento(string nome, string numeroSerie)
         {
-            for (int i = 0; i < list.Length; i++)
+            try
             {
-                if (list[i].Nome == nome && list[i].Serie == Convert.ToInt32( numeroSerie))
+                for (int i = 0; i < list.Length; i++)
                 {
-                    return list[i];
+                    if (list[i].Nome == nome && list[i].Serie == Convert.ToInt32(numeroSerie))
+                    {
+                        return list[i];
+                    }
                 }
+                return null;
             }
-            return null;
+            catch (Exception)
+            {
+                Console.WriteLine("Valor Invalido");
+                return null;
+            }
         }
 
         public void EditarEquipamento(Equipamento equipamentOriginal)
@@ -78,7 +174,6 @@ namespace GestãoEquipamentos_Acade2021
             Console.WriteLine("Insira o Novo Fabricante: ");
             equipamentOriginal.Fabricante = Console.ReadLine();
 
-
         }
         public void AdicionarEquipamento(Equipamento equipamento)
         {
@@ -91,21 +186,30 @@ namespace GestãoEquipamentos_Acade2021
                 }
             }
         }
-        public void RemoverEquipamento(string nome)
+
+        public void RemoverEquipamento(string nome, string numeroSerie)
         {
-            for (int i = 0; i < list.Length; i++)
+            try
             {
-                if (list[i].Nome == nome)
+                for (int i = 0; i < list.Length; i++)
                 {
-                    list[i] = null;
-                    return;
+                    if (list[i].Nome == nome && list[i].Serie == Convert.ToInt32(numeroSerie))
+                    {
+                        list[i] = null;
+                        return;
+                    }
                 }
             }
+            catch (Exception)
+            {
+                Console.WriteLine("Valor Invalido");
+            }
         }
+
         public void TexteAdicionarEquipamento()
         {
             Equipamento x = new Equipamento(10, "leonardo", 2, "10/10/2021", "veronica");
-            x.Id = new Random().Next(0,10000);
+            x.Id = new Random().Next(0, 10000);
             AdicionarEquipamento(x);
         }
         public void CriarEquipamento()
@@ -127,7 +231,7 @@ namespace GestãoEquipamentos_Acade2021
             Console.WriteLine("Insira o Fabricante: ");
             equipamento.Fabricante = Console.ReadLine();
             //Criando um Id
-            equipamento.Id = new Random().Next(0,10000);
+            equipamento.Id = new Random().Next(0, 10000);
             //adicionando
             AdicionarEquipamento(equipamento);
         }
